fce70eec3f93355d752bc18b2905fc1388f806bf
Sandi_Navoiy <santes.au@gmail.com>
2023-10-30 22:00:27 +0300

    фильтр по городу

--- retail_network/admin.py
+++ retail_network/admin.py
@@ -8,9 +8,22 @@ from retail_network.models import Retail_network
 @admin.register(Retail_network)
 class RetailAdmin(admin.ModelAdmin):
     list_display = ("title", "contacts", "debt")
-    date_hierarchy = "release_date"  # иерархия по датам
+    date_hierarchy = "release_date"
+    list_filter = ("contacts__city",)  # Фильтр по полю city
     actions = ("cancel_debt",)
+
+    # Определение уникальных значений городов
+    def get_city_choices(self, request):
+        """Определение уникальных значений городов"""
+        return Retail_network.objects.values_list("contacts__city", flat=True).distinct()
+
+    def formfield_for_choice_field(self, db_field, request, **kwargs):
+        """настройки виджета и выбора полей результатов в админке Django"""
+        if db_field.name == "contacts__city":
+            kwargs['choices'] = self.get_city_choices(request)
+        return super().formfield_for_choice_field(db_field, request, **kwargs)
 
     @admin.action(description="Сбросить задолженность")
     def cancel_debt(self, request, queryset: QuerySet):
         queryset.update(debt=0.00)
+
